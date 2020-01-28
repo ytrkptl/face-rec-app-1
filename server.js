b5f4ecd63778c2d016f9c25cf658a6c4d8acd767
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-01-27 21:13:11 -0500

    moved server content to root folder and .env to root folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const morgan = require('morgan');
 
 // comes with Express.
@@ -42,10 +42,10 @@ app.use(cors())
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   });
 }
 
