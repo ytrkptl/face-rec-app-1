8fe60de4f5d2f383783c352522dc79ebfcb8824b
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-01-28 20:16:12 -0500

    removed console.log statements from profileicon.js and app.js

--- client/src/components/Profile/ProfileIcon.js
+++ client/src/components/Profile/ProfileIcon.js
@@ -7,62 +7,11 @@ import {
 } from 'reactstrap';
 import './ProfileIcon.css';
 
-// class ProfileIcon extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 			dropdownOpen: false,
-// 		}
-// 	}
-
-// 	componentDidUpdate() {
-// 		console.log('received')
-// 	}
-
-
-// 	toggle = () => {
-// 		this.setState(prevState => ({
-// 			dropdownOpen: !prevState.dropdownOpen
-// 		}));
-// 	}
-
-// 	render() {
-// 		const { profilePhotoUrl } = this.props;
-// 		return (
-// 			<div className="dropdownParentDiv">
-// 				<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-// 					<DropdownToggle
-// 						tag="span"
-// 						data-toggle="dropdown"
-// 						aria-expanded={this.state.dropdownOpen}>
-// 						<img
-// 							className="homeAvatarImage"
-// 							src={profilePhotoUrl}
-// 							alt="avatar" />
-// 					</DropdownToggle>
-// 					<DropdownMenu
-// 						right
-// 						className="dropdownMenuStyle"
-// 						/*needed to inject some styles directly*/
-// 						style={{ marginTop: '10px', right: 0, backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
-// 						<DropdownItem onClick={() => this.props.toggleModal()}>View Profile</DropdownItem>
-// 						<DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
-// 					</DropdownMenu>
-// 				</Dropdown>
-// 			</div>
-// 		);
-// 	}
-// }
-
 const ProfileIcon = ({ profilePhotoUrl, toggleModal, onRouteChange }) => {
 
 	const [dropdownOpen, setDropdownOpen] = useState(false);
 
 	const toggle = () => setDropdownOpen(prevState => !prevState);
-
-	useEffect(() => {
-		console.log(profilePhotoUrl)
-	}, [profilePhotoUrl])
 
 	return (
 		<div className="dropdownParentDiv">
