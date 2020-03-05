08ec5299346310fff2f2f6ee22d265de63b6bdf0
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-03-04 19:57:10 -0500

    removing the randomId that was set for debugging only from register-step-1.js

--- controllers/register/register-step-1.js
+++ controllers/register/register-step-1.js
@@ -43,8 +43,7 @@ to the user if key does exist in redis already up to 5 times.
 the limit is set to 5 to keep anyone from abusing the system by requesting multiple
 emails*/
 const checkIfEmailExistsInRedis = (name, email, passwordEnc, req, res) => {
-  // const randomId = randomIdFunc();
-  const randomId = 'aaaaaaaa';
+  const randomId = randomIdFunc();
   let someKeys = ['randomId', 'name', email, 'password', 'email', 'requestCount']
   let someVals = [randomId, name, randomId, passwordEnc, email, 1]
 
