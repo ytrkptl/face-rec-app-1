cd9e77066e464bb7f03872ff85566bcb064f0c72
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-05-24 21:10:43 -0400

    aligned htmlFor and form ids for better accessibility; added service worker; add 512 x 512 and 192 x 192 favicons

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+});
+
 app.use(routes);
 
 app.listen(port, (error) => {
