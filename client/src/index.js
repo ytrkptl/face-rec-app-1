a188bdcdf3b2c6bf67a8eae71562fb06503e311a
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2024-09-15 15:32:56 -0400

    Remove serviceWorker from client

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./containers/App";
-import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
@@ -12,4 +11,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-serviceWorker.register();
