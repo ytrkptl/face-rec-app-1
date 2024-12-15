7eb3c4f00cdc260e4d7516e50ac4487ba1a6d8dc
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2024-12-14 20:38:01 -0500

    Added the error controller.

---
+++ controllers/error/error.js
@@ -0,0 +1,19 @@
+const errorHandler = async (error, req, res, next) => {
+  let doc = {
+    messageForLog: error.messageForLog,
+    messageSent: error.message,
+  };
+  console.log(doc)
+  // for cases when we want to send success message despite of error
+  // in backend.
+  if (error.status === 200) {
+    return res.status(200).json(error.message);
+  }
+  return res.status(error.status || 500).json({
+    error: {
+      message: error.message || "Oops! Something went wrong.",
+    },
+  });
+};
+
+module.exports = errorHandler;
